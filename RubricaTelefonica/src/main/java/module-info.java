88c21291ee4b8da5c34d5820d65be0139750457b
Giovanni <Giovanni@DESKTOP-AMCIOKJ>
2024-11-25 10:46:58 +0100

    App iniziale

---
+++ RubricaTelefonica/src/main/java/module-info.java
@@ -0,0 +1,9 @@
+/* doesn't work with source level 1.8:
+module com.mycompany.rubricatelefonica {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    opens com.mycompany.rubricatelefonica to javafx.fxml;
+    exports com.mycompany.rubricatelefonica;
+}
+*/
