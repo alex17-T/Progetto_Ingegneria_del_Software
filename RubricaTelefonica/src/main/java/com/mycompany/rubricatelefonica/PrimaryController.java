55b433b1705daa258e0400d41caeec6b83e87d1a
alessandro <alessandro@DESKTOP-5KTDMKN>
2024-12-02 11:26:13 +0100

    adattamento dimensioni

--- RubricaTelefonica/src/main/java/com/mycompany/rubricatelefonica/PrimaryController.java
+++ RubricaTelefonica/src/main/java/com/mycompany/rubricatelefonica/PrimaryController.java
@@ -1,12 +1,70 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
 package com.mycompany.rubricatelefonica;
 
-import java.io.IOException;
+import com.mycompany.rubricatelefonica.*;
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 
-public class PrimaryController {
+/**
+ * FXML Controller class
+ *
+ * @author alessandro
+ */
+public class PrimaryController implements Initializable {
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
 
     @FXML
-    private void switchToSecondary() throws IOException {
-        App.setRoot("secondary");
+    private void AggiungiContatto(ActionEvent event) {
     }
+
+    @FXML
+    private void ModificaFotoContatto(ActionEvent event) {
+    }
+
+    @FXML
+    private void ModificaContatto(ActionEvent event) {
+    }
+
+    @FXML
+    private void RimuoviFotoProfilo(ActionEvent event) {
+    }
+
+    @FXML
+    private void ImportaLista(ActionEvent event) {
+    }
+
+    @FXML
+    private void EsportaLista(ActionEvent event) {
+    }
+
+    @FXML
+    private void ModificaListaContatti(ActionEvent event) {
+    }
+
+    @FXML
+    private void SelezionaTuttoCheckBox(ActionEvent event) {
+    }
+
+    @FXML
+    private void RimoviContattiSelezionati(ActionEvent event) {
+    }
+
+    @FXML
+    private void CercaElemento(ActionEvent event) {
+    }
+    
 }
