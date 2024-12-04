74cfef4f0a5fec85de857eff1aa4d3216150c6f8
Giovanni <Giovanni@DESKTOP-AMCIOKJ>
2024-12-04 14:49:43 +0100

    Il bottone di modifica rende visibile e invisibile la toolbar

--- RubricaTelefonica/src/main/java/com/mycompany/rubricatelefonica/HomeController.java
+++ RubricaTelefonica/src/main/java/com/mycompany/rubricatelefonica/HomeController.java
@@ -6,13 +6,20 @@
 package com.mycompany.rubricatelefonica;
 
 import com.mycompany.rubricatelefonica.*;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.HBox;
+import javafx.stage.FileChooser;
+import javafx.stage.FileChooser.ExtensionFilter;
 
 /**
  * FXML Controller class
@@ -25,6 +32,8 @@ public class HomeController implements Initializable {
     private HBox ToolBar;
     @FXML
     private HBox SerchBar;
+    @FXML
+    private ImageView contactImage;
 
     /**
      * Initializes the controller class.
@@ -40,7 +49,13 @@ public class HomeController implements Initializable {
     }
 
     @FXML
-    private void ModificaFotoContatto(ActionEvent event) {
+    private void ModificaFotoContatto(ActionEvent event) throws FileNotFoundException {
+        /*FileChooser fc = new FileChooser();
+        fc.getExtensionFilters().addAll(
+        new ExtensionFilter("Image Files", "*.png", "*.jpg", "*.gif"));
+        File file = fc.showOpenDialog(null);
+        Image img = new Image(new FileInputStream(file));
+        contactImage.imageProperty().set(img);*/
     }
 
     @FXML
@@ -50,6 +65,8 @@ public class HomeController implements Initializable {
 
     @FXML
     private void RimuoviFotoProfilo(ActionEvent event) {
+        /*Image defImg = new Image("@FotoProfiloDefault.png");
+        contactImage.setImage(defImg);*/
     }
 
     @FXML
@@ -84,6 +101,7 @@ public class HomeController implements Initializable {
 
     @FXML
     private void AnnullaOperazione(ActionEvent event) {
+        ToolBar.visibleProperty().set(false);
     }
     
 }
