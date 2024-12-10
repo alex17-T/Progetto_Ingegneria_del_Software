031f8455c5bcf17e07b618e03082d7b1ac5b873a
Giovanni <Giovanni@DESKTOP-AMCIOKJ>
2024-12-10 20:23:01 +0100

    Risolto un bug nel pulsante annulla dentro alla toolbar

--- RubricaTelefonica/src/main/java/com/mycompany/rubricatelefonica/HomeController.java
+++ RubricaTelefonica/src/main/java/com/mycompany/rubricatelefonica/HomeController.java
@@ -663,6 +663,7 @@ public class HomeController implements Initializable{
     @FXML
     private void AnnullaOperazione(ActionEvent event) {
         ToolBar.visibleProperty().set(false);
+        Colonna_Spunta.setVisible(!Colonna_Spunta.visibleProperty().get());
     }
 
     /**
