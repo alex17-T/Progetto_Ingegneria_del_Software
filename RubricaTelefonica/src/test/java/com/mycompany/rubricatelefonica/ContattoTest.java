8a73f97a84758c173d36f102558d0309930fd25a
Giovanni <Giovanni@DESKTOP-AMCIOKJ>
2024-12-13 23:57:09 +0100

    Aggiunte le dipendenze per i test j-unit

--- RubricaTelefonica/src/test/java/com/mycompany/rubricatelefonica/ContattoTest.java
+++ RubricaTelefonica/src/test/java/com/mycompany/rubricatelefonica/ContattoTest.java
@@ -80,7 +80,7 @@ public class ContattoTest {
         assertEquals(expResult, result);
         // TODO review the generated test code and remove the default call to fail.
         fail("The test case is a prototype.");
-    }
+        }
 
     /**
      * Test of setFotoprofilo method, of class Contatto.
